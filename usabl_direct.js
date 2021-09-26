774f8c7342bbcaf9b67f0ad356a3b308d1033dcc
Gopal K. Vashishtha <gopalvashishtha@college.harvard.edu>
2021-09-25 22:58:58 -0700

    adding files

---
+++ usabl_direct.js
@@ -0,0 +1,203 @@
+const _0x38bca3 = _0x2af0;
+(function (_0x2a9efd, _0x58bdf2) {
+  const _0xcd41e4 = _0x2af0,
+    _0x3e231a = _0x2a9efd();
+  while (!![]) {
+    try {
+      const _0x27b222 =
+        parseInt(_0xcd41e4(0x1d6)) / 0x1 +
+        parseInt(_0xcd41e4(0x1e1)) / 0x2 +
+        (-parseInt(_0xcd41e4(0x1dc)) / 0x3) *
+          (-parseInt(_0xcd41e4(0x1d4)) / 0x4) +
+        (parseInt(_0xcd41e4(0x1c1)) / 0x5) *
+          (-parseInt(_0xcd41e4(0x1d1)) / 0x6) +
+        (parseInt(_0xcd41e4(0x1c7)) / 0x7) *
+          (-parseInt(_0xcd41e4(0x1d7)) / 0x8) +
+        (-parseInt(_0xcd41e4(0x1e7)) / 0x9) *
+          (parseInt(_0xcd41e4(0x1d5)) / 0xa) +
+        (parseInt(_0xcd41e4(0x1c6)) / 0xb) * (parseInt(_0xcd41e4(0x1e4)) / 0xc);
+      if (_0x27b222 === _0x58bdf2) break;
+      else _0x3e231a["push"](_0x3e231a["shift"]());
+    } catch (_0x46fd54) {
+      _0x3e231a["push"](_0x3e231a["shift"]());
+    }
+  }
+})(_0x1ef7, 0x6a67a),
+  document[_0x38bca3(0x1ca)](_0x38bca3(0x1db), function (_0x20dbc3) {
+    addElement();
+  });
+function _0x1ef7() {
+  const _0x122981 = [
+    "className",
+    "focus",
+    "src",
+    "display",
+    "none",
+    "media",
+    "Prompt",
+    "body",
+    "block",
+    "Powered-by-Usabl",
+    "cssText",
+    "https://app.usabl.dev/record?originURL=",
+    "innerText",
+    "float:right;bottom:2%;right:1%;position:fixed;z-index:2147483647",
+    "open",
+    "link",
+    "https://cdn.jsdelivr.net/gh/Usabl-dev/usabl_direct_public/usabl_video.png",
+    "mouseleave",
+    "style",
+    "https://usabl.dev/?utm_campaign=viral&utm_source=embed_link",
+    "createElement",
+    "Powered\x20by",
+    "origin\x20URL\x20is\x20",
+    "stylesheet",
+    "695iUukCc",
+    "rel",
+    "text-style-1",
+    "div",
+    "img",
+    "11dsesoa",
+    "3976XxZWsy",
+    "text/css",
+    "test",
+    "addEventListener",
+    "Record-your-screen",
+    "onclick",
+    "all",
+    "Img",
+    "https://fonts.googleapis.com/css?family=Roboto",
+    "Record-button",
+    "33072LFqgBv",
+    "span",
+    "target",
+    "14148ASsOyK",
+    "34780epWaGu",
+    "412652bYYcdP",
+    "3280hbjlQN",
+    "href",
+    "type",
+    "appendChild",
+    "DOMContentLoaded",
+    "615fEJnXq",
+    "innerHTML",
+    "location",
+    ">Usabl",
+    "mouseenter",
+    "1113484tziuTh",
+    "head",
+    "log",
+    "348924WkqwzH",
+    "hostname",
+    "_blank",
+    "747CtTRVv",
+  ];
+  _0x1ef7 = function () {
+    return _0x122981;
+  };
+  return _0x1ef7();
+}
+function _0x2af0(_0x5e0c80, _0x15b60e) {
+  const _0x1ef79d = _0x1ef7();
+  return (
+    (_0x2af0 = function (_0x2af0fe, _0xf56e5f) {
+      _0x2af0fe = _0x2af0fe - 0x1ad;
+      let _0x5a2f4a = _0x1ef79d[_0x2af0fe];
+      return _0x5a2f4a;
+    }),
+    _0x2af0(_0x5e0c80, _0x15b60e)
+  );
+}
+function addElement() {
+  const _0x285c3f = _0x38bca3;
+  var _0x5183aa = /iphone|ipod|android|blackberry|mini|windows\sce|palm/i[
+    _0x285c3f(0x1c9)
+  ](navigator["userAgent"]["toLowerCase"]());
+  if (_0x5183aa) return;
+  const _0x385574 = document["createElement"](_0x285c3f(0x1c4));
+  _0x385574[_0x285c3f(0x1bb)][_0x285c3f(0x1b3)] = _0x285c3f(0x1b6);
+  var _0x23e6c9 =
+      "\x0a\x20\x20.Img\x20{\x0a\x20\x20\x20\x20border-radius:\x2050%;\x20\x0a\x20\x20\x20\x20width:\x205.0vw;\x20\x0a\x20\x20\x20\x20height:\x20auto;\x20\x0a\x20\x20\x20\x20border:20%\x20solid\x20white;\x0a\x20\x20\x20\x20-webkit-border-radius:\x2050%;\x0a\x20\x20\x20\x20-moz-border-radius:\x2050%;\x0a\x20\x20\x20\x20box-shadow:\x200px\x204px\x204px\x20rgba(0,\x200,\x200,\x200.25);\x0a\x20\x20\x20\x20cursor:\x20pointer\x0a\x20\x20}\x0a\x20\x20.Overlay\x20{\x0a\x20\x20\x20\x20width:\x20403px;\x0a\x20\x20\x20\x20height:\x20222px;\x0a\x20\x20\x20\x20padding:\x2033.6px\x2013px\x2021.8px\x2020.5px;\x0a\x20\x20\x20\x20border-radius:\x2020px;\x0a\x20\x20\x20\x20background-color:\x20white;\x0a\x20\x20\x20\x20text-align:\x20center;\x0a\x20\x20\x20\x20box-shadow:\x200px\x204px\x204px\x20rgba(0,\x200,\x200,\x200.25);\x20\x0a\x20\x20}\x0a\x20\x20.Prompt\x20{\x0a\x20\x20\x20\x20width:\x20341px;\x0a\x20\x20\x20\x20height:\x2049.7px;\x0a\x20\x20\x20\x20margin:\x200\x2028.6px\x2030.6px\x200;\x0a\x20\x20\x20\x20font-family:\x20Roboto;\x0a\x20\x20\x20\x20font-size:\x2024px;\x0a\x20\x20\x20\x20font-weight:\x20900;\x0a\x20\x20\x20\x20font-stretch:\x20normal;\x0a\x20\x20\x20\x20font-style:\x20normal;\x0a\x20\x20\x20\x20line-height:\x20normal;\x0a\x20\x20\x20\x20letter-spacing:\x20normal;\x0a\x20\x20\x20\x20text-align:\x20center;\x0a\x20\x20\x20\x20color:\x20#000;\x0a\x20\x20}\x0a\x0a\x20\x20.Record-button\x20{\x0a\x20\x20\x20\x20width:\x20319.7px;\x0a\x20\x20\x20\x20height:\x2050.6px;\x0a\x20\x20\x20\x20flex-grow:\x200;\x0a\x20\x20\x20\x20margin:\x2030.6px\x2028.3px\x2014.6px\x2021.5px;\x0a\x20\x20\x20\x20padding:\x2010px\x2040.7px\x206px\x2040.7px;\x0a\x20\x20\x20\x20border-radius:\x205px;\x0a\x20\x20\x20\x20background-color:\x20#1890ff;\x0a\x20\x20\x20\x20display:\x20flex;\x0a\x20\x20}\x0a\x0a\x20\x20.Record-your-screen\x20{\x0a\x20\x20\x20\x20width:\x20239.1px;\x0a\x20\x20\x20\x20height:\x2033.8px;\x0a\x20\x20\x20\x20flex-grow:\x200;\x0a\x20\x20\x20\x20font-family:\x20Roboto;\x0a\x20\x20\x20\x20font-size:\x2024px;\x0a\x20\x20\x20\x20font-weight:\x20bold;\x0a\x20\x20\x20\x20font-stretch:\x20normal;\x0a\x20\x20\x20\x20font-style:\x20normal;\x0a\x20\x20\x20\x20line-height:\x20normal;\x0a\x20\x20\x20\x20letter-spacing:\x20normal;\x0a\x20\x20\x20\x20text-align:\x20center;\x0a\x20\x20\x20\x20color:\x20white;\x0a\x20\x20}\x0a\x0a\x20\x20.Powered-by-Usabl\x20{\x0a\x20\x20\x20\x20width:\x20154px;\x0a\x20\x20\x20\x20height:\x2021px;\x0a\x20\x20\x20\x20flex-grow:\x200;\x0a\x20\x20\x20\x20margin:\x2014.6px\x200\x200\x20200.5px;\x0a\x20\x20\x20\x20font-family:\x20Roboto;\x0a\x20\x20\x20\x20font-size:\x2015px;\x0a\x20\x20\x20\x20font-weight:\x20bold;\x0a\x20\x20\x20\x20font-stretch:\x20normal;\x0a\x20\x20\x20\x20font-style:\x20normal;\x0a\x20\x20\x20\x20line-height:\x20normal;\x0a\x20\x20\x20\x20letter-spacing:\x20normal;\x0a\x20\x20\x20\x20text-align:\x20left;\x0a\x20\x20\x20\x20color:\x20rgba(0,\x200,\x200,\x200.5);\x0a\x20\x20}\x0a\x20\x20.Powered-by-Usabl\x20.text-style-1\x20{\x0a\x20\x20\x20\x20color:\x20rgba(5,\x20136,\x203,\x200.5);\x0a\x20\x20\x20\x20margin:\x200\x200\x200\x204px;\x0a\x20\x20}",
+    _0x329383 = document[_0x285c3f(0x1bd)](_0x285c3f(0x1bb));
+  (_0x329383[_0x285c3f(0x1d9)] = _0x285c3f(0x1c8)),
+    (_0x329383[_0x285c3f(0x1b5)] = _0x23e6c9),
+    document[_0x285c3f(0x1e2)][_0x285c3f(0x1da)](_0x329383);
+  var _0x2cb8d1 = "myCss";
+  if (!document["getElementById"](_0x2cb8d1)) {
+    var _0xcb2509 = document["getElementsByTagName"](_0x285c3f(0x1e2))[0x0],
+      _0x50d998 = document[_0x285c3f(0x1bd)](_0x285c3f(0x1b8));
+    (_0x50d998["id"] = _0x2cb8d1),
+      (_0x50d998[_0x285c3f(0x1c2)] = _0x285c3f(0x1c0)),
+      (_0x50d998[_0x285c3f(0x1d9)] = _0x285c3f(0x1c8)),
+      (_0x50d998[_0x285c3f(0x1d8)] = _0x285c3f(0x1cf)),
+      (_0x50d998[_0x285c3f(0x1ae)] = _0x285c3f(0x1cd)),
+      _0xcb2509[_0x285c3f(0x1da)](_0x50d998);
+  }
+  let _0x554edd = document[_0x285c3f(0x1bd)](_0x285c3f(0x1c5));
+  (_0x554edd[_0x285c3f(0x1ea)] = _0x285c3f(0x1b9)),
+    (_0x554edd[_0x285c3f(0x1e8)] = _0x285c3f(0x1ce));
+  const _0x17d445 = document["createElement"](_0x285c3f(0x1c4));
+  _0x17d445["className"] = "Overlay";
+  const _0x184dd0 = document["createElement"](_0x285c3f(0x1d2));
+  (_0x184dd0["className"] = _0x285c3f(0x1af)),
+    (_0x184dd0["innerHTML"] =
+      "Want\x20to\x20provide\x20feedback\x20directly\x20to\x20the\x20product\x20team?"),
+    _0x17d445[_0x285c3f(0x1da)](_0x184dd0);
+  const _0x414135 = document["createElement"]("a"),
+    _0x365ea2 = window[_0x285c3f(0x1de)][_0x285c3f(0x1e5)];
+  (_0x414135[_0x285c3f(0x1d8)] = _0x285c3f(0x1b4) + _0x365ea2),
+    (_0x414135[_0x285c3f(0x1d3)] = _0x285c3f(0x1e6));
+  const _0xde532c = document[_0x285c3f(0x1bd)](_0x285c3f(0x1c4)),
+    _0x20826e = document[_0x285c3f(0x1bd)]("div");
+  _0x20826e[_0x285c3f(0x1e8)] = _0x285c3f(0x1d0);
+  const _0x3eee1c = document[_0x285c3f(0x1bd)](_0x285c3f(0x1d2));
+  (_0x3eee1c["className"] = _0x285c3f(0x1cb)),
+    (_0x3eee1c[_0x285c3f(0x1dd)] = "Record\x20your\x20screen"),
+    _0xde532c[_0x285c3f(0x1da)](_0x20826e),
+    _0x20826e[_0x285c3f(0x1da)](_0x3eee1c),
+    _0x414135[_0x285c3f(0x1da)](_0xde532c),
+    _0x17d445["appendChild"](_0x414135);
+  const _0x5d1d43 = document[_0x285c3f(0x1bd)](_0x285c3f(0x1d2));
+  (_0x5d1d43["className"] = _0x285c3f(0x1b2)),
+    (_0x5d1d43[_0x285c3f(0x1dd)] = _0x285c3f(0x1be));
+  const _0x315003 = document["createElement"]("a");
+  (_0x315003[_0x285c3f(0x1d8)] = _0x285c3f(0x1bc)),
+    (_0x315003[_0x285c3f(0x1d3)] = _0x285c3f(0x1e6));
+  const _0x395eec = document["createElement"](_0x285c3f(0x1d2));
+  (_0x395eec[_0x285c3f(0x1e8)] = _0x285c3f(0x1c3)),
+    (_0x395eec["innerHTML"] = _0x285c3f(0x1df)),
+    _0x315003[_0x285c3f(0x1da)](_0x395eec),
+    _0x5d1d43[_0x285c3f(0x1da)](_0x315003),
+    _0x17d445[_0x285c3f(0x1da)](_0x5d1d43),
+    (_0x17d445["style"][_0x285c3f(0x1eb)] = _0x285c3f(0x1ad)),
+    _0x385574["appendChild"](_0x17d445),
+    _0x554edd["addEventListener"](
+      _0x285c3f(0x1e0),
+      function (_0x4c7c30) {
+        const _0x1c6b06 = _0x285c3f;
+        (_0x17d445["style"][_0x1c6b06(0x1eb)] = _0x1c6b06(0x1b1)),
+          (_0x554edd[_0x1c6b06(0x1bb)][_0x1c6b06(0x1eb)] = "none");
+      },
+      ![]
+    ),
+    _0x17d445[_0x285c3f(0x1ca)](
+      _0x285c3f(0x1ba),
+      function (_0x11ccbf) {
+        const _0x250a7d = _0x285c3f;
+        (_0x17d445[_0x250a7d(0x1bb)][_0x250a7d(0x1eb)] = _0x250a7d(0x1ad)),
+          (_0x554edd[_0x250a7d(0x1bb)][_0x250a7d(0x1eb)] = "block");
+      },
+      ![]
+    ),
+    (_0x554edd[_0x285c3f(0x1cc)] = () => {
+      const _0x5c5fc9 = _0x285c3f,
+        _0x1aac64 = window[_0x5c5fc9(0x1de)][_0x5c5fc9(0x1e5)];
+      console[_0x5c5fc9(0x1e3)](_0x5c5fc9(0x1bf) + _0x1aac64),
+        window[_0x5c5fc9(0x1b7)](
+          _0x5c5fc9(0x1b4) + _0x1aac64,
+          _0x5c5fc9(0x1e6)
+        )[_0x5c5fc9(0x1e9)]();
+    }),
+    _0x385574["appendChild"](_0x554edd),
+    document[_0x285c3f(0x1b0)][_0x285c3f(0x1da)](_0x385574);
+}
